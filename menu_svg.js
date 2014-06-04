e79bbe5c15b232444298c5ee8d6097ea7b7a8c48
Rémi Andrieux <pimolo10300@gmail.com>
2014-06-04 10:55:17 +0200

    classes js

---
+++ menu_svg.js
@@ -0,0 +1,15 @@
+var calques = $('polygon');
+console.log(calques);
+calques.each(function() {
+  $(this).click(function() {
+    calques.attr('class', '');
+    var clicked = $(this);
+    console.log(clicked[0]);
+    // TODO : supprimer ckicked de calques[]
+    for (var i = calques.length - 1; i >= 0; i--) {
+      if (clicked[0] !== $(calques)[i]) {
+        $(calques[i]).attr('class', 'anim' + (i+1));
+      };
+    };
+  });
+});
