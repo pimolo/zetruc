1a49d91b1b1d1e6598ca5d2a14b3533580f8d04b
Rémi Andrieux <pimolo10300@gmail.com>
2014-06-04 23:53:03 +0200

    beau svg + reverse anime

--- menu_svg.js
+++ menu_svg.js
@@ -1,15 +1,19 @@
-var calques = $('g');
-console.log(calques);
-calques.each(function() {
-  $(this).click(function() {
-    calques.removeAttr('class');
-    var clicked = $(this);
-    console.log(clicked[0]);
-    // TODO : supprimer ckicked de calques[]
-    for (var i = calques.length - 1; i >= 0; i--) {
-      if (clicked[0] !== $(calques)[i]) {
-        $(calques[i]).attr('class', 'anim' + (i+1));
+$(document).ready(function() {
+  $('body').addClass('reverse');
+  var calques = $('g');
+  console.log(calques);
+  calques.each(function() {
+    $(this).click(function() {
+      calques.removeAttr('class');
+      var clicked = $(this);
+      $('body').toggleClass('reverse');
+      console.log(clicked[0]);
+      // TODO : supprimer ckicked de calques[]
+      for (var i = calques.length - 1; i >= 0; i--) {
+        if (clicked[0] !== $(calques)[i]) {
+          $(calques[i]).attr('class', 'anim' + (i+1));
+        }
       };
-    };
+    });
   });
 });
