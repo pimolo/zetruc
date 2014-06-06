ee65f793a5bf5ed41da2842e5e9ee5536aa3bcd5
Rémi Andrieux <pimolo10300@gmail.com>
2014-06-06 11:23:38 +0200

    transitions

--- menu_svg.js
+++ menu_svg.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
-  alert("Les technologies utilisées dans ce site ne sont pas très bien supportées par Firefox et IE.\nMerci d'utiliser un navigateur à jour tel que Google Chrome.");
   $('body').addClass('reverse');
   var calques = $('g');
-  console.log(calques);
   calques.each(function() {
     $(this).click(function() {
-      calques.removeAttr('class');
       var clicked = $(this);
+      calques.removeAttr('class');
       $('body').toggleClass('reverse');
-      console.log(clicked[0]);
-      // TODO : supprimer ckicked de calques[]
+      $('#contact path').toggle('slow');
+      $('#presentation text:last-child').toggle('slow');
       for (var i = calques.length - 1; i >= 0; i--) {
         if (clicked[0] !== $(calques)[i]) {
           $(calques[i]).attr('class', 'anim' + (i+1));
